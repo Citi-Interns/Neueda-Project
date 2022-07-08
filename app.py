d1410cc498fb5829755be940887a1c44a17ccde1
Nitish Agrawal <nitishagrawal612@gmail.com>
2022-07-08 10:40:57 +0530

    change env

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from routes import *
 from flask import Flask
+import os
 
 def main():
     app = Flask(__name__)
     app.register_blueprint(routes)
-    app.run(host='0.0.0.0', port=80)
+    IS_DEV = app.env == 'development'
+    os.environ['FLASK_ENV'] = 'development'
+    app.run(host='0.0.0.0',debug=True, port=80)
 
 if __name__ == "__main__" :
     main()
